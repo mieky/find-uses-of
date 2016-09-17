d68ff77966f667b7c6a19a0c74d545da80ee8be1
Mike Arvela <mike.arvela@futurice.com>
2016-09-17 23:20:00 +0300

    List packages using specified module

--- index.js
+++ index.js
@@ -2,16 +2,13 @@
 const path = require('path');
 const promise = require('bluebird');
 const fs = promise.promisifyAll(require('fs'));
+const has = require('lodash.has');
 
 // 1. find all first-level package.json files recursively
 //    (if current directory contains it, will not walk subdirectories)
 // 2. see which of those contains the wanted module as a dependency
 
-// package.json found in current directory?
-// - yes: see if package.json contains the wanted module
-// - no: walk subdirectories
-
-const findLocationsWithPackageJson = promise.coroutine(function* find(foundLocations, dir) {
+const findSubdirsWithPackageJson = promise.coroutine(function* findSubdirs(foundLocations, dir) {
   const locations = foundLocations || [];
 
   const files = yield fs.readdirAsync(dir);
@@ -21,7 +18,7 @@ const findLocationsWithPackageJson = promise.coroutine(function* find(foundLocat
     return locations.concat(dir);
   }
 
-  // search subdirectories recursively
+  // package.json wasn't found yet, search subdirectories recursively
   const dirs = yield promise
     .map(files, f => path.resolve(dir, f))
     .filter(filePath => fs.statAsync(filePath)
@@ -31,12 +28,31 @@ const findLocationsWithPackageJson = promise.coroutine(function* find(foundLocat
         return false;
       })
     )
-    .map(d => findLocationsWithPackageJson(locations, d));
+    .map(d => findSubdirsWithPackageJson(locations, d));
 
   // flatten a potential [[], [], ['dir']] mess into a neat array
   return locations.concat(...dirs);
 });
 
-findLocationsWithPackageJson(null, process.cwd())
-  .then(locations => console.log(locations))
+function isNeedleInHaystack(haystackDir, needleName) {
+  const pkgJsonPath = path.resolve(haystackDir, 'package.json');
+
+  /* eslint-disable global-require */
+  const pkgJson = require(pkgJsonPath);
+  return has(pkgJson, `dependencies.${needleName}`) ||
+    has(pkgJson, `devDependencies.${needleName}`);
+  /* eslint-enable global-require */
+}
+
+if (process.argv.length < 3) {
+  console.log('Usage: find-using <module>');
+  process.exit(1);
+}
+
+const needle = process.argv[2];
+console.log(`Finding packages depending on ${needle}...`);
+
+findSubdirsWithPackageJson(null, process.cwd())
+  .filter(location => isNeedleInHaystack(location, needle))
+  .then(locations => locations.forEach(l => console.log(l)))
   .catch(err => console.log(err));
